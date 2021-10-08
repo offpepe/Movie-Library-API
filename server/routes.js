7815493c04012dc8383a9bbc9ecee8d921d3f6fc
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-08 12:08:51 -0400

    feat: validations on createNewUser

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const userValidation = require('../validations/users');
 const usersControllers = require('../controllers/users');
 
 /* USERS ROUTES */
-router.post('/users', usersControllers.createUser);
+router.post('/users', userValidation.validateNewUserData, usersControllers.createUser);
 
 module.exports = router;
