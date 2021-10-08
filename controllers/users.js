0b4baa57c267862dcee42bb437f1ef284a7f923e
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-08 13:57:25 -0400

    feat: loginUser middleware

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,20 @@
+const STATUS = require('../services/httpStatus');
 const usersService = require('../services/users');
 
 const createUser = async (req, res) => {
-    const { username, email, password } = req.body;
-    const op = await usersService.createUser(username, email, password);
-    return res.status(201).json(op);
+  const { username, email, password } = req.body;
+  const op = await usersService.createUser(username, email, password);
+  return res.status(STATUS.SUCCESS.CREATED).json(op);
 };
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  const data = await usersService.loginUser(email, password);
+  if (!data) res.status(STATUS.ERROR.UNPROCESSABLE_ENTITY).json({ message: 'in progress' });
+  res.status(STATUS.SUCCESS.ACCEPTED).json(data);
+}
 
 module.exports = {
     createUser,
+    loginUser,
 };
