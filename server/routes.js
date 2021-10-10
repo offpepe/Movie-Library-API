29aafb6bbe462d5ce032619fa6d1497e3ff74766
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-10 00:30:24 -0400

    feat: validate id on update movie

--- server/routes.js
+++ server/routes.js
@@ -22,7 +22,7 @@ router.post('/movies/create',
   moviesControllers.createMovie
 );
 router.get('/movies/', moviesValidations.validateToken, moviesControllers.getMovies);
-router.put('/movies/update', uploadMovie, moviesValidations.validateUpdatedFields,moviesControllers.updateMovie);
+router.put('/movies/update', moviesValidations.validateId, uploadMovie, moviesValidations.validateUpdatedFields,moviesControllers.updateMovie);
 router.delete('/movies/delete/:id', moviesValidations.validateId, moviesControllers.deleteMovie);
 
 module.exports = router;  
