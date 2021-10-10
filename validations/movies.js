3640ad3890d311ed85665a70e10662375cc436e9
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-10 13:00:48 -0400

    fix: token validation, changing some to every

--- validations/movies.js
+++ validations/movies.js
@@ -20,16 +20,16 @@ const validateNewMovieData = (req, res, next) => {
 
 const validateToken = async (req, res, next) => {
   const { authorization } = req.headers;
-  console.log(authorization);
   const rawTokens = await fs.readFile('./assets/tokens.json', 'utf-8');
   const tokens = JSON.parse(rawTokens);
-  if(!authorization || tokens.some((token) => token !== authorization)) return res.status(STATUS.ERROR.UNAUTHORIZED).json(ERROR.notLoggedIn);
+  console.log(tokens);
+  if(!authorization || tokens.every((token) => token !==  authorization)) return res.status(STATUS.ERROR.UNAUTHORIZED).json(ERROR.notLoggedIn);
   next()
 };
 
 const validateUpdatedFields = async (req, res, next) => {
   const { id, title, subtitle, description } = req.body;
-  if(!id || !title || !subtitle || !description ) return res.status(STATUS.ERROR.UNPROCESSABLE_ENTITY).json(ERROR.someFieldEmpty);
+  if(!id || !title || !subtitle || !description ) return res.status(STATUS.ERROR.UNPROCESSABLE_ENTITY).json(ERROR.someFieldEmty);
   if (!req.file) return res.status(STATUS.ERROR.UNPROCESSABLE_ENTITY).json(ERROR.fileNotFoundCover);
   next();
 }
