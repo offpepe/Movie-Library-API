4e50bcc9036526c3cd83c2acb235de9a959791d1
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-12 00:08:49 -0400

    fix: uncomment createMovie validation

--- validations/movies.js
+++ validations/movies.js
@@ -5,18 +5,16 @@ const ERROR = require('../error/messages');
 
 const validateNewMovieData = (req, res, next) => {
   const { title, subtitle, description } = req.body;
-  console.log(req.body);
-  console.log(req.file);
-  // const { mimetype, filename } = req.file;
-  // const type = mimetype.split('/');
-  // if (!title || !subtitle || !description) {
-  //   fs.unlinkSync(`./uploads/movies/${filename}`);
-  //   return res.status(STATUS.ERROR.UNPROCESSABLE_ENTITY).json(ERROR.newMovieData);
-  // }
-  // if (type[1] !== 'jpeg') {
-  //   fs.unlinkSync(`./uploads/movies/${filename}`);  
-  //   return res.status(STATUS.ERROR.NOT_ACCEPTABLE).json(ERROR.fileTypeNotAcceptable);
-  // }
+  const { mimetype, filename } = req.file;
+  const type = mimetype.split('/');
+  if (!title || !subtitle || !description) {
+    fs.unlinkSync(`./uploads/movies/${filename}`);
+    return res.status(STATUS.ERROR.UNPROCESSABLE_ENTITY).json(ERROR.newMovieData);
+  }
+  if (type[1] !== 'jpeg') {
+    fs.unlinkSync(`./uploads/movies/${filename}`);  
+    return res.status(STATUS.ERROR.NOT_ACCEPTABLE).json(ERROR.fileTypeNotAcceptable);
+  }
   next();
 };
 
