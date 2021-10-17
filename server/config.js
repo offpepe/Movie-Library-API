068ce7e6db152d4bb952a859ce9d5ae687e764a4
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-17 13:37:24 -0400

    feat: process.env.port as api port

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ const router = require('./routes');
 const cors = require('cors');
 const app = express();
 
-const PORT = process.env.API_KEY || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
